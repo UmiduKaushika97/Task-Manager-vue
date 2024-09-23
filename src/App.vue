d45b8072a0fc75974a9466494e0e1c00c28a429f
UmiduKaushika97 <umidu.kaushika@gmail.com>
2024-09-23 09:44:02 +0530

    Task Manager app complete

---
+++ src/App.vue
@@ -0,0 +1,133 @@
+<script setup>
+import { ref, reactive, computed }  from 'vue';
+import Task from './components/Task.vue';
+import Filter from './components/Filter.vue';
+import ModalWindow from './components/modal/ModalWindow.vue';
+import AddTaskModal from './components/modal/AddTaskModal.vue';
+import {useTasksStore} from './stores/tasksStore.js'
+const appName = "Tasks Manager";
+
+const store = useTasksStore();
+
+store.$subscribe((mutation, state) => {
+
+  // persist the whole state to the local storage whenever it changes
+  localStorage.setItem('tasks', JSON.stringify(state.tasks))
+})
+
+</script>
+
+<template>
+
+  <main class="container">
+    <div class="header">
+      <div class="header-side">
+        <h1>
+          {{appName}}
+        </h1>
+      </div>
+
+      <div class="header-side">
+        <div>
+          <button @click="store.openModal" class="btn secondary">+ Add Task</button>
+        </div>
+      </div>
+
+      <!-- <input type="text" v-model="appName"> -->
+    </div>
+    
+   <Filter />
+
+    <div class="tasks">
+      
+      <Task v-for="(task, index ) in store.filteredTasks" :task="task" :key="index" />
+      
+
+      <!-- <div class="task">
+        <h3>
+          Website development
+        </h3>
+        <p>
+          Develop the portfolio website using Vue JS.
+        </p>
+        <div class="task-check">
+          <input type="checkbox"/>
+          <label>
+            To-Do
+          </label>
+        </div>
+      </div> -->
+
+    </div>
+
+
+  <ModalWindow v-if="store.modalIsActive">
+    <AddTaskModal/>
+  </Modalwindow>
+
+  </main>
+  
+   
+
+</template>
+
+
+<style lang="scss" scoped>
+
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  .header-side {
+    display: flex;
+    align-items: center;
+
+    h1 {
+      text-transform: capitalize;
+      font-size: 42px;
+      font-weight: 700;
+      line-height: 47px;
+      letter-spacing: 0em;
+      text-align: left;
+    }
+
+    .secondary {
+      margin-left: 12px;
+    }
+  }
+
+}
+
+
+
+.tasks {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 20px;
+
+  @media (max-width: 768px) {
+    grid-template-columns: repeat(1, 1fr);
+  }
+}
+
+
+
+.add-task {
+  margin-top: 60px;
+
+  input, textarea {
+    width: 360px;
+    max-width: 100%;
+    margin-top: 12px;
+    padding: 5px;
+  }
+
+  button {
+    width: 360px;
+    margin-top: 12px;
+  }
+}
+
+
+</style>
